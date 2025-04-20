d86eb1a566824d6c4910fb1e44fdd970ec996e65
AugustoAguinaga <apaguinagal@gmail.com>
2025-04-20 03:25:58 -0500

    Climas
    Cambios en enviroment. Creacion de funciones para clima.

--- Scripts/enviroment.gd
+++ Scripts/enviroment.gd
@@ -1,27 +1,28 @@
 extends Node2D
 
 #Variables Tiempo
-@export var min_dia = 30
+@export var min_dia = 1
 var ciclo_dia_noche = min_dia
 var dia:bool = true
 
 #Variables Clima : [viento minimo, viento max]
-var climas = {
+var dic_climas = {
 	"depejado" : [0, 50], 
 	 "tormenta" : [80, 150],
 	"lluvioso" : [30, 100],
 	"niebla" : [0, 20]
 }
 
-
+var clima = dic_climas.keys()[0]
 
 func _ready() -> void:
+	
 	pass
-
 
 func _process(delta: float) -> void:
 	cambio_ciclo_dia_noche(delta)
 	disco_dia_noche(delta)
+	print (clima)
 
 func cambio_ciclo_dia_noche(delta):
 	if ciclo_dia_noche > 0 :
@@ -32,6 +33,9 @@ func cambio_ciclo_dia_noche(delta):
 	else: 
 		ciclo_dia_noche = min_dia
 		dia = !dia
+		pool_climas()
+		#clima = pool_climas() 
+		viento()
 		print(dia, ciclo_dia_noche)
 
 func disco_dia_noche(delta):
@@ -39,10 +43,15 @@ func disco_dia_noche(delta):
 	#var omega = (PI * 2) / (2 * min_dia) 
 	$Dia_Noche.rotation += (PI * 2) / (2 * min_dia) * delta
 
-func clima( d:Dictionary ):
+func pool_climas():
 	var rng = RandomNumberGenerator.new()
-	rng.randomize()
-	var vals = d.values()  # Array de valores
-	print()
-	return vals[rng.randi_range(0, vals.size() - 1)]
+	var claves = dic_climas.keys()
+	var idx = rng.randi_range(0 , claves.size() -1)
+	var clima = claves[idx]
+	print(clima)
+
+func viento():
+	var array_clima = dic_climas[clima] as Array
+	var viento_min = array_clima[0]
+	print("EL viento_minimo es ", viento_min)
 	
