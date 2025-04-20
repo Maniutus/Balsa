4638b86d34eeb41f25f5a7eeae89998ecacfb2eb
AugustoAguinaga <apaguinagal@gmail.com>
2025-04-19 19:35:34 -0500

    Correcciones
    Olas y agregar eviroment

--- Scripts/enviroment.gd
+++ Scripts/enviroment.gd
@@ -1,28 +1,48 @@
 extends Node2D
 
-const min_dia = 30
+#Variables Tiempo
+@export var min_dia = 30
 var ciclo_dia_noche = min_dia
 var dia:bool = true
+
+#Variables Clima : [viento minimo, viento max]
+var climas = {
+	"depejado" : [0, 50], 
+	 "tormenta" : [80, 150],
+	"lluvioso" : [30, 100],
+	"niebla" : [0, 20]
+}
+
 
 
 func _ready() -> void:
 	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	cambio_ciclo_dia_noche(delta)
+	disco_dia_noche(delta)
 
 func cambio_ciclo_dia_noche(delta):
 	if ciclo_dia_noche > 0 :
 		
 		ciclo_dia_noche -= delta
-		print(ciclo_dia_noche)
+		#print(ciclo_dia_noche)
 		
 	else: 
 		ciclo_dia_noche = min_dia
 		dia = !dia
 		print(dia, ciclo_dia_noche)
-		
-	 
+
+func disco_dia_noche(delta):
+		#const TAU = PI * 2
+	#var omega = (PI * 2) / (2 * min_dia) 
+	$Dia_Noche.rotation += (PI * 2) / (2 * min_dia) * delta
+
+func clima( d:Dictionary ):
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var vals = d.values()  # Array de valores
+	print()
+	return vals[rng.randi_range(0, vals.size() - 1)]
 	
