cdde7e9220eb290301ddbb2fd76e7c95785d2b95
Maniutus <taller.relojeriac3@gmail.com>
2025-04-20 05:41:12 -0600

    enviromen funciones de vemocidad viento, angulo viento

--- Scripts/enviroment.gd
+++ Scripts/enviroment.gd
@@ -1,30 +1,38 @@
 extends Node2D
 
 #Variables Tiempo
-@export var min_dia = 1
+@export var min_dia = 5
 var ciclo_dia_noche = min_dia
 var dia:bool = true
+var nombre_dia = "Es de Dia"
 
-#Variables Clima : [viento minimo, viento max]
+#Variables Clima : [viento minimo, viento max, altura de olas, luvia:bool ]
 var dic_climas = {
-	"depejado" : [0, 50], 
+	"depejado" : [0, 50 ], 
 	 "tormenta" : [80, 150],
 	"lluvioso" : [30, 100],
 	"niebla" : [0, 20]
 }
 
+
 var clima = dic_climas.keys()[0]
+@export var dir_viento = 0
+
+
 
 func _ready() -> void:
+	print (clima)
+	direccion_viento()
+	velocidad_viento ()
+	print(nombre_dia)
 	
-	pass
 
 func _process(delta: float) -> void:
 	cambio_ciclo_dia_noche(delta)
-	disco_dia_noche(delta)
-	print (clima)
+	animacion_sprites(delta)
 
 func cambio_ciclo_dia_noche(delta):
+	
 	if ciclo_dia_noche > 0 :
 		
 		ciclo_dia_noche -= delta
@@ -34,24 +42,56 @@ func cambio_ciclo_dia_noche(delta):
 		ciclo_dia_noche = min_dia
 		dia = !dia
 		pool_climas()
-		#clima = pool_climas() 
-		viento()
-		print(dia, ciclo_dia_noche)
+		direccion_viento() 
+		velocidad_viento()
+		if dia:
+			nombre_dia = "Es de Dia"
+			print(nombre_dia)
+		else:
+			nombre_dia = "Es de Noche"
+			print(nombre_dia)
 
-func disco_dia_noche(delta):
-		#const TAU = PI * 2
-	#var omega = (PI * 2) / (2 * min_dia) 
-	$Dia_Noche.rotation += (PI * 2) / (2 * min_dia) * delta
+func animacion_sprites(delta):
+	$Dia_Noche.rotation += (PI * 2) / (2 * min_dia) * delta # Gira el sprite del disco
+	
+	$Viento.rotation = dir_viento
+	var raw:int = $Viento.rotation
+	
+	#print("Angulo flecha: ", dir_viento," y ",raw)
 
 func pool_climas():
 	var rng = RandomNumberGenerator.new()
 	var claves = dic_climas.keys()
 	var idx = rng.randi_range(0 , claves.size() -1)
-	var clima = claves[idx]
+	var clima_elegida = claves[idx]
+	clima = clima_elegida
 	print(clima)
 
-func viento():
+func velocidad_viento():
+
 	var array_clima = dic_climas[clima] as Array
 	var viento_min = array_clima[0]
-	print("EL viento_minimo es ", viento_min)
+	var viento_max = array_clima[1]
+	
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var velocidad_viento = rng.randi_range(viento_min, viento_max)
+	#print("EL viento_min es: ", viento_min)
+	#print("EL viento_max es: ", viento_max)
+	print("La velocidad del viento es: ", velocidad_viento)
+	
+func direccion_viento():
+	var rango_max = 10
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var dir_viento_raw = rng.randi_range(dir_viento-rango_max, dir_viento+rango_max)
+	dir_viento = ((dir_viento_raw % 360) + 360) % 360 
+	print("Direccion Viento: ", dir_viento, " grados")
+	
+	
+func cambio_dir_viento():
+	pass
+	
+func cambio_fuerza_viento():
+	pass
 	
